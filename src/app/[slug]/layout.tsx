1bffc4e943ac66f8b7bf3f2688f38f0cba005d13
osamuel-cardoso <samuelcardoso555@gmail.com>
2025-07-06 04:56:21 -0300

    Update layout.tsx

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -4,9 +4,6 @@ import { Header } from '@/components/Header'
 export default async function Layout({
     children,
 }: Readonly<{
-    params: {
-        slug: string
-    }
     children: React.ReactNode
 }>) {
     return (
